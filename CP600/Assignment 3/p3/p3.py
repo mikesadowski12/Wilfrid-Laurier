bfe376931a257dedccf4f1eda361363f87489e4a
Mike Sadowski <msadowsk@opentext.com>
2021-05-20 18:13:32 -0400

    assignment 3 problem 3

--- CP600/Assignment 3/p3/p3.py
+++ CP600/Assignment 3/p3/p3.py
@@ -29,27 +29,35 @@ def Build_Max_Heap(list, length):
     Max_Heapify(list, length, i)
 
 # =================================================================================================
+# Explanation:
+#   1. Loop until k numbers are inputted from the stream, store all temperatures in an array
+#   2. When k temperatures have been inputted, store the final temperature and transform input array
+#      into a max heap
+#   3. As more temperatures are inputted, compare the root (which will be the k-th smallest temperature seen so far)
+#      to the current inputted temperature. If the current temperature is smaller, replace root with the current
+#      temperature and heapify the array to get the largest temperature to the root (k-th smallest temperature)
+# =================================================================================================
 count = 0
 list = []
 k = int(input("Enter integer for k: "))
 
 while 1:
-  value = int(input("Enter an integer: "))
+  temperature = int(input("Enter a temperature: "))
 
-  # append to list for first k elements
+  # append to list for first k temperatures
   if (count < k - 1):
-    list.append(value)
+    list.append(temperature)
   else:
-    if count == k - 1: # found the k-th value, build a heap
-      list.append(value)
+    if count == k - 1: # found the k-th temperature, build a heap
+      list.append(temperature)
       Build_Max_Heap(list, len(list))
     else:
-      if (value < list[0]): # check if current k-th value is smaller than current value
+      if (temperature < list[0]): # check if current k-th temperature is smaller than current temperature
         # replace root if necessary and re-heapify
-        list[0] = value
+        list[0] = temperature
         Build_Max_Heap(list, len(list))
 
-    print("list is ", list)
-    print("k-th smallest value is ", list[0])
+    # k-th smallest temperature is always the root of the max-heap
+    print("k-th smallest temperature is ", list[0])
 
   count += 1
