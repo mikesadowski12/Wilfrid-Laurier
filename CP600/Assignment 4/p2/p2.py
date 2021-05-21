bb18bfe1272d796ac8f6fc37d95bc5a2fae08040
Mike Sadowski <msadowsk@opentext.com>
2021-05-20 22:32:16 -0400

    Assignment 4 Problem 2

---
+++ CP600/Assignment 4/p2/p2.py
@@ -0,0 +1,49 @@
+def swapNodes(list, pos1, pos2):
+  temp = list[pos1]
+  list[pos1] = list[pos2]
+  list[pos2] = temp
+
+# Taken from Assignment 3 Problem 1
+def Partition(A, lo, hi): # Partition A[lo..hi] using A[hi] as pivot
+  x = A[hi] # x = A[hi] is the pivot element
+  i = (lo - 1) # A[lo .. i] is the ≤ x
+
+  # A[i+1 .. j-1] are greater than x
+  for j in range(lo, hi):
+    if A[j] <= x: # compare current element A[j] with x
+      i = i + 1 # if A[j] ≤ x, then left array is increased
+      swapNodes(A, i, j) # > x array is shifted one place and A[j] is added to ≤x array
+
+  # Exchange A[i+1] ← A[hi]
+  swapNodes(A, i + 1, hi)
+
+  # return i + 1
+  return i + 1
+
+# Taken from Assignment 3 Problem 1
+def QuickSort(A, lo, hi):
+  if len(A) <= 1:
+    return A
+
+  # if lo < hi: there are more than one elements
+  if lo < hi:
+    q = Partition(A, lo, hi) # Partition A into ≤x and >x sub-arrays
+
+    QuickSort(A, q + 1, hi) # Recurs on right array, i.e., > x array
+    QuickSort(A, lo, q - 1) # Recurs on left array, i.e. ≤ x array
+
+def InsertionSort():
+  print("InsertionSort")
+
+def QuickInsertionSort(A, n, k):
+  if n > k:
+    QuickSort(A, 0, len(A) - 1)
+
+  if n <= k:
+    InsertionSort(A, 0, len(A) - 1)
+
+inputList = [104, 226, 3, 7, 69, 77, 144, 15, 29, 30, 31]
+
+print("Input:", inputList)
+QuickInsertionSort(inputList, len(inputList) - 1, 100)
+print("Output:", inputList)
