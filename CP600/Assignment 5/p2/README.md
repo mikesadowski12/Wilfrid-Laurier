7c792d43fd85b3469319abcfa5537199f3143fc5
Mike Sadowski <msadowsk@opentext.com>
2021-05-26 17:01:54 -0400

    Assignment 5 Problem 2

--- CP600/Assignment 5/p2/README.md
+++ CP600/Assignment 5/p2/README.md
@@ -1,8 +1,8 @@
 # Assignment 5
 ## Problem 2:
 * Run `python3 p2.py` inside this directory
-* sample input: ``
-* sample output: ``
+* sample input: `Balanced Tree 1, Unbalanced Tree 2`
+* sample output: `Tree 1 isBalanced() is  True, Tree 2 isBalanced() is  False`
 
 ## Notes:
 * Ran assignment using `python3` command (shown above)
---
+++ CP600/Assignment 5/p2/p2.py
@@ -0,0 +1,61 @@
+class Node:
+  def __init__(self, k):
+    self.left = None
+    self.right = None
+    self.key = k
+
+def buildBalancedTestTree():
+  root = Node(11)
+
+  root.left = Node(3)
+  root.left.left = Node(1)
+  root.left.right = Node(9)
+
+  root.right = Node(15)
+  root.right.left = Node(12)
+  root.right.right = Node(14)
+
+  return root
+
+def buildNotBalancedTestTree():
+  root = Node(11)
+
+  root.left = Node(3)
+  root.left.left = Node(1)
+  root.left.right = Node(9)
+  root.left.left.left = Node(12)
+
+  root.right = Node(15)
+
+  return root
+
+def getHeight(root):
+  if root:
+    heightLeft = getHeight(root.left)
+    heightRight = getHeight(root.right)
+
+    return max(heightLeft, heightRight) + 1
+
+  return 0
+
+def isBalanced(root):
+  if root is None:
+    return True
+
+  heightLeft = getHeight(root.left)
+  heightRight = getHeight(root.right)
+
+  difference = abs(heightLeft - heightRight) # 1, -1, 0
+  isLeftBalanced = isBalanced(root.left)
+  isRightBalanced = isBalanced(root.right)
+
+  if isLeftBalanced and isRightBalanced and (difference <= 1):
+    return True
+
+  return False
+
+tree1 = buildBalancedTestTree()
+tree2 = buildNotBalancedTestTree()
+
+print("Tree 1 isBalanced() is ", isBalanced(tree1))
+print("Tree 2 isBalanced() is ", isBalanced(tree2))
