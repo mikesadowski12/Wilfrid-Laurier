d993f872a4337add39b67fcc7ad4d805922ed1c6
Mike Sadowski <msadowsk@opentext.com>
2021-05-27 16:54:57 -0400

    Assignment 4 Problem 2

--- CP600/Assignment 4/p2/p2.py
+++ CP600/Assignment 4/p2/p2.py
@@ -88,7 +88,7 @@ print("testCase3 Input:", testCase3)
 QuickInsertionSort(testCase3, len(testCase3), 0)
 print("testCase3 Output:", testCase3)
 print("----------------------------------------")
-testCase4 = [1, 2, 3]
+testCase4 = [1, 3, -2]
 print("testCase4 Input:", testCase4)
 QuickInsertionSort(testCase4, len(testCase4), 3)
 print("testCase4 Output:", testCase4)
