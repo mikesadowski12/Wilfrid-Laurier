605545d5c224f2ab6f5f44597134e212fd29debe
Mike Sadowski <msadowsk@opentext.com>
2021-06-09 15:21:04 -0400

    Assignment 5 Problem 3

--- CP600/Assignment 5/p3/p3.py
+++ CP600/Assignment 5/p3/p3.py
@@ -4,7 +4,7 @@ class Node:
     self.right = None
     self.key = k
 
-def buildTestTree():
+def buildTestTree1():
   root = Node(25)
 
   root.left = Node(10)
@@ -20,6 +20,23 @@ def buildTestTree():
 
   return root
 
+def buildTestTree2():
+  root = Node(100)
+
+  root.left = Node(20)
+  root.left.left = Node(30)
+  root.left.right = Node(40)
+  root.left.left.right = Node(10)
+  root.left.right.left = Node(11)
+  root.left.right.left = Node(12)
+
+  root.right = Node(130)
+  root.right.left = Node(14)
+  root.right.right = Node(15)
+
+  return root
+
+
 def BST_LCA(t, x, y):
   if t is None:
     return None
@@ -32,7 +49,10 @@ def BST_LCA(t, x, y):
 
   return t
 
-root = buildTestTree()
-node = BST_LCA(root, 6, 19)
+root1 = buildTestTree1()
+node1 = BST_LCA(root1, 6, 19)
+print("Test 1 - Output: LCA is ", node1.key)
 
-print("Output: LCA is ", node.key)
+root2 = buildTestTree2()
+node2 = BST_LCA(root2, 60, 190)
+print("Test 2 - Output: LCA is ", node2.key)
