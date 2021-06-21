d8142a62d28614f8f77654c4f99f2c5267e97312
Mike Sadowski <msadowsk@opentext.com>
2021-06-21 19:31:52 -0400

    Assignment 10 Problem 1

--- CP600/Assignment 10/p1/p1.py
+++ CP600/Assignment 10/p1/p1.py
@@ -1,25 +1,23 @@
 def longestPalindrome(s):
   if not s:
-    return 0
+    return ''
 
   n = len(s)
-  matrix = [[0] * n for _ in range(n)]
+  matrix = [['0'] * n for _ in range(n)]
 
-  # Initialize diagonal of matrix to 1's (each letters own start position to its self ending position)
-  # Length of palindrome is 1 since string is 1 character long
+  # Initialize diagonal of matrix to each letter (own start position to its self ending position)
   for i in range(n):
-    matrix[i][i] = 1
+    matrix[i][i] = s[i]
 
   for i in range(2, n + 1):
     for j in range(n - i + 1):
       k = j + i - 1
 
       if s[j] == s[k]:
-        matrix[j][k] = matrix[j + 1][k - 1] + 2
-
+        matrix[j][k] = s[j] + matrix[j + 1][k - 1] + s[k]
       else:
-        matrix[j][k] = max(matrix[j + 1][k - 1], matrix[j + 1][k], matrix[j][k - 1])
+        matrix[j][k] = max([matrix[j + 1][k - 1], matrix[j + 1][k], matrix[j][k - 1]], key = len)
 
   return matrix[0][n - 1]
 
-print("Longest palindrome is: ", longestPalindrome("character"))
+print("longest palindrome in \'character\':", longestPalindrome("character"))
