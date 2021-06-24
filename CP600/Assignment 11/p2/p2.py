f6af1f4ce3b2b37ff2094933be434e0de8a6f30e
Mike Sadowski <msadowsk@opentext.com>
2021-06-24 14:21:37 -0400

    Assignemnt 11 Problem 2

--- CP600/Assignment 11/p2/p2.py
+++ CP600/Assignment 11/p2/p2.py
@@ -15,30 +15,51 @@ def hamiltonianCycle(Graph, nodeCount, currentNode):
         return cycle
 
 testCase1Graph = {
-  1:[2, 3, 4],
-  2:[1, 3, 4],
-  3:[1, 2, 4],
-  4:[1, 2, 3],
+  1:[2],
+  2:[3],
+  3:[4],
+  4:[1],
 }
 
 testCase2Graph = {
   1:[2],
-  2:[3, 4],
-  4:[3],
+  2:[3],
+  3:[4],
+  4:[2],
 }
 
 testCase3Graph = {
-  1:[2, 3, 4],
-  2:[1, 3, 4],
-  3:[1, 2, 4],
-  4:[1, 2, 3],
+  1:[],
+  2:[],
+  3:[],
+  4:[],
+}
+
+testCase4Graph = {
+  1:[3, 4],
+  2:[4],
+  3:[1, 2],
+  4:[1],
+}
+
+testCase5Graph = {
+  1:[3, 4],
+  2:[4],
+  3:[1, 2],
+  4:[1],
 }
 
 visited = []
-print("Test Case 1 Hamiltonian cycle:", hamiltonianCycle(testCase1Graph, len(testCase1Graph), 3))
+print("Test Case 1 Hamiltonian cycle:", hamiltonianCycle(testCase1Graph, len(testCase1Graph), 1))
 
 visited = []
-print("Test Case 2 Hamiltonian cycle:", hamiltonianCycle(testCase2Graph, len(testCase2Graph), 1))
+print("Test Case 2 Hamiltonian cycle:", hamiltonianCycle(testCase2Graph, len(testCase2Graph), 4))
 
 visited = []
 print("Test Case 3 Hamiltonian cycle:", hamiltonianCycle(testCase3Graph, len(testCase3Graph), 2))
+
+visited = []
+print("Test Case 4 Hamiltonian cycle:", hamiltonianCycle(testCase4Graph, len(testCase4Graph), 2))
+
+visited = []
+print("Test Case 5 Hamiltonian cycle:", hamiltonianCycle(testCase5Graph, len(testCase5Graph), 3))
