501892fdfa7a68b01b997285673005483e6e9bda
Mike Sadowski <msadowsk@opentext.com>
2021-06-24 14:14:48 -0400

    Assignemnt 11 Problem 2

--- CP600/Assignment 11/p2/p2.py
+++ CP600/Assignment 11/p2/p2.py
@@ -1,4 +1,6 @@
-def hamiltonianCycle(Graph, nodeCount, currentNode, visited = []):
+visited = []
+
+def hamiltonianCycle(Graph, nodeCount, currentNode):
   if currentNode not in visited:
     visited.append(currentNode)
 
@@ -7,10 +9,10 @@ def hamiltonianCycle(Graph, nodeCount, currentNode, visited = []):
       return visited
 
     for nextNode in Graph[currentNode]:
-      candidate = hamiltonianCycle(Graph, nodeCount, nextNode)
+      cycle = hamiltonianCycle(Graph, nodeCount, nextNode)
 
-      if candidate is not None:
-        return candidate
+      if cycle:
+        return cycle
 
 testCase1Graph = {
   1:[2, 3, 4],
@@ -25,5 +27,18 @@ testCase2Graph = {
   4:[3],
 }
 
-print("Test Case 1 Hamiltonian cycle:", hamiltonianCycle(testCase1Graph, len(testCase1Graph), 2))
-print("Test Case 2 Hamiltonian cycle:", hamiltonianCycle(testCase2Graph, len(testCase2Graph), 2))
+testCase3Graph = {
+  1:[2, 3, 4],
+  2:[1, 3, 4],
+  3:[1, 2, 4],
+  4:[1, 2, 3],
+}
+
+visited = []
+print("Test Case 1 Hamiltonian cycle:", hamiltonianCycle(testCase1Graph, len(testCase1Graph), 3))
+
+visited = []
+print("Test Case 2 Hamiltonian cycle:", hamiltonianCycle(testCase2Graph, len(testCase2Graph), 1))
+
+visited = []
+print("Test Case 3 Hamiltonian cycle:", hamiltonianCycle(testCase3Graph, len(testCase3Graph), 2))
